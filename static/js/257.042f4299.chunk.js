b60e6c1ea9e02122403e66d10da25d5a71f50cb1
AlexFilp <110115726+AlexFilp@users.noreply.github.com>
2023-09-08 12:48:22 +0000

    Deploying to gh-pages from @ AlexFilp/goit-react-hw-08-phonebook@de0b414f4649265f179688a00b3bcc993264d701 🚀

---
+++ static/js/257.042f4299.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[257],{828:function(t,e,n){n.d(e,{Ie4:function(){return a}});var r=n(9983);function a(t){return(0,r.w_)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M8 15.5l7.5-7.5h-4.5v-8h-6v8h-4.5z"}}]})(t)}},2144:function(){}}]);
+//# sourceMappingURL=257.042f4299.chunk.js.map
